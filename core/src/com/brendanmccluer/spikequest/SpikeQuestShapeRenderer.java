de1b0478d421d4650caef1eecd843c9d26cda8fd
Brendan McCluer <brendanmccluer@live.com>
2017-04-08 18:23:32 -0500

    -Created Safe Dispose -Fixed Balloon Game -Fixed Gravity Issue

---
+++ core/src/com/brendanmccluer/spikequest/SpikeQuestShapeRenderer.java
@@ -0,0 +1,33 @@
+package com.brendanmccluer.spikequest;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.brendanmccluer.spikequest.cameras.SpikeQuestCamera;
+
+/**
+ * Created by brend on 4/7/2017.
+ */
+
+public class SpikeQuestShapeRenderer {
+    ShapeRenderer shapeRenderer = null;
+
+    public SpikeQuestShapeRenderer() {
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    public void drawRectangle(Rectangle rectangle, SpikeQuestCamera camera) {
+        if (rectangle != null && camera != null) {
+            shapeRenderer.setAutoShapeType(true);
+            shapeRenderer.setProjectionMatrix(camera.getProjectionMatrix());
+            shapeRenderer.setColor(Color.BLACK);
+            shapeRenderer.begin();
+            shapeRenderer.box(rectangle.x, rectangle.y, 0, rectangle.width, rectangle.height, 0);
+            shapeRenderer.end();
+        }
+    }
+
+    public void discard() {
+        shapeRenderer.dispose();
+    }
+}
